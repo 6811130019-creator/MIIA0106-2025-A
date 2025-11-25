483ffc1d6317736fcf4e6f3016415a7e92464552
6811130019-creator <6811130019@mut.ac.th>
2025-11-25 16:54:15 +0700

    การทำลอฃครั้งที่ 2

---
+++ LAB02/Test01/Test01.cpp
@@ -0,0 +1,10 @@
+// Test01.cpp
+
+#include <iostream>
+
+int main() {
+    std::cout << "Name: Sumet Khamdara" << std::endl;
+    std::cout << "Course: MIIA0106" << std::endl;
+    std::cout << "Email: 6811130019@mut.ac.th" << std::endl;
+    return 0;
+}
