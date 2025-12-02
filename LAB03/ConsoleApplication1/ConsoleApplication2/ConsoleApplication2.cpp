234e1b49434d45dee4e6158f06e7567732801b7a
6811130019-creator <6811130019@mut.ac.th>
2025-12-02 17:26:03 +0700

    ส่งงาน

--- LAB03/ConsoleApplication1/ConsoleApplication2/ConsoleApplication2.cpp
+++ LAB03/ConsoleApplication1/ConsoleApplication2/ConsoleApplication2.cpp
@@ -6,32 +6,33 @@ int main() {
     std::string Name;
     int Score;
     char grade;
-    
+
     std::cout << "Enter Student ID: ";
     std::cin >> studentID;
     std::cin.ignore(); // To ignore the newline character after entering studentID
     std::cout << "Enter Name:";
     std::cin >> Name;
-    do
+    // รับคะแนนจนกว่าจะถูกต้อง
+    while (true)
     {
         std::cout << "Enter Score:";
         std::cin >> Score;
         if (Score >= 0 && Score <= 100)
-			break;
-		else
-			std::cout << "Invalid Score! Please enter a score between 0 and 100." << std::endl;
-    } while (Score >= 0 && Score <= 100);
+            break;
+        else
+            std::cout << "Invalid Score! Please enter a score between 0 and 100." << std::endl;
+    }
     if (Score >= 90)
-		grade = 'A';
-	else if (Score >= 80)
-		grade = 'B';
-	else if (Score >= 70)
-		grade = 'C';
-	else if (Score >= 60)
-		grade = 'D';
-	else
-		grade = 'F';
-    
+        grade = 'A';
+    else if (Score >= 80)
+        grade = 'B';
+    else if (Score >= 70)
+        grade = 'C';
+    else if (Score >= 60)
+        grade = 'D';
+    else
+        grade = 'F';
+
     std::cout << "-------Student Report-------" << std::endl;
     std::cout << "Student ID  : " << studentID << std::endl;
     std::cout << "Name        : " << Name << std::endl;
