8cbc4e85b467b399745532aaa72dc93953ebce27
6811130019-creator <6811130019@mut.ac.th>
2025-12-16 15:43:35 +0700

    1

--- LAB04/ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
+++ LAB04/ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -3,10 +3,13 @@
 using namespace std;
 
 int main() {
-	int i;
-	for (i = 1; i < 11; i++)
-	{
-		cout << i << endl;
-	}
-	return 0;
+	int sum = 0;
+	int i = 0;
+	while (i < 100)
+		{
+		sum += i;
+		i += 3;
+		
+		}
+	cout << sum << endl;
 }
